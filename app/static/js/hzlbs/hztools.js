2dd5af9973f96b6963a36cd608bd0cbe941a370d
anningwang <wxg001@21cn.com>
2018-01-18 10:12:33 +0800

    js文件改名，文件合并。

--- app/static/js/hzlbs/hztools.js
+++ app/static/js/hzlbs/hztools.js
@@ -132,3 +132,17 @@ function gritter_alert(title, text) {
     });
     return false;
 }
+
+
+var _locOrigin = {'x':0,'y':0};          // 定位坐标原点
+var _locRange = {'x':39023,'y':19854};   // 定位范围
+
+// 坐标转换 简化公式
+var _mapOrigin = {'x':12531716.588,'y':3101784.7414};
+var _mapRange = {'x':12531761.921,'y':3101761.9051};
+function hzPlaneCoordToFMap(coord) {
+    var x = (coord.x - _locOrigin.x)/ (_locRange.x - _locOrigin.x) * (_mapRange.x - _mapOrigin.x) + _mapOrigin.x;
+    var y = (coord.y - _locOrigin.y) / (_locRange.y - _locOrigin.x) * (_mapRange.y - _mapOrigin.y) + _mapOrigin.y;
+    
+    return {'x': x, 'y': y};
+}
