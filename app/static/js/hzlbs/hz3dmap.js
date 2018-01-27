3c6900459448030a0cd7fc777b29dc8ad58018a2
anningwang <wxg001@21cn.com>
2018-01-27 11:53:12 +0800

    3Dmap： 实现导航功能。

--- app/static/js/hzlbs/hz3dmap.js
+++ app/static/js/hzlbs/hz3dmap.js
@@ -26,110 +26,23 @@
 		var self = this;
 		
 		this.tools = HzTools;
-		this.hz_marker = {};     // {userId: {mark: p}
 		
-		this.container = options.container;     // JQuery 对象
+		this.container = options.container; // JQuery 对象
 		this.restoreService = new Init();   // 业务控制面板按钮间初始化对象
 		this.serviceCtrlPanelId = 'hz_service_ctrl_panel';
 		this.fmapContainerId = options.fmapContainerId;
+		this.isNavigating = false;  // 是否处于模拟导航状态
+		this.destCoord = undefined;  // 导航目的地
+		this.userList = {};          // 用户列表 { userId: HzPeople }
 
 		// 创建 3D 地图
 		this.createFmap();
 
-		// 地图加载完成回掉方法
-		this.fmap.on('loadComplete',function() {
-			var toolControl = new fengmap.toolControl(self.fmap, {
-				// 初始化2D模式
-				init2D: false,
-				// 设置为false表示只显示2D,3D切换按钮
-				groupsButtonNeeded: false,
-				imgURL: '/static/img/',
-				// 点击按钮的回调方法,返回type表示按钮类型,value表示对应的功能值
-				clickCallBack: function(type, value) {
-					// console.log(type,value);
-				}
-			});
-			console.log('toolControl', toolControl);
-			
-			self.socket = io.connect(Hzlbs.HZ_CONN_STR);
-			// 增加定位代码，实时显示人物位置
-			self.socket.on('hz_position', function (msg) {
-				console.log('hz_position', msg);
-				for(var i = 0; i < msg.length; i++) {
-					var coord = {x: msg[i].x, y: msg[i].y};
-					var fm_coord = Hzlbs.coordMapToFMap(coord);
-					if (msg[i].userId in self.hz_marker) {
-						self.hz_marker[msg[i].userId].marker.moveTo({
-							x: fm_coord.x,
-							y: fm_coord.y
-						});
-					} else {
-						var color = 'blue';
-						if (HzTools.selectUserId == msg[i].userId) color = 'red';
-						var im = self.addMarker(fm_coord, color);
-						if (im) {
-							self.hz_marker[msg[i].userId] = {marker: im};
-						}
-					}
-				}
-			});
-		});
-		
+		// 地图加载完成回调方法
+		this.loadComplete();
 
-		var naviCoord = [];
 		// 地图点击事件
-		this.fmap.on('mapClickNode',function(event) {
-			if (event.nodeType == fengmap.FMNodeType.MODEL) {
-				var modelLabel = event.label;
-				var coord;
-
-				// 如果拾取的模型没有Label对象，则使用模型中心点的坐标
-				// 有则使用与模型对应的Label对象的坐标。
-				if (!modelLabel) {
-					coord = {
-						x: event.mapCoord.x,
-						y: event.mapCoord.y,
-						groupID: event.groupID
-					};
-					console.log('no label', coord);
-				} else {
-					coord = {
-						x: modelLabel.mapCoord.x,
-						y: modelLabel.mapCoord.y,
-						groupID: event.groupID
-					};
-
-					console.log('has label', coord);
-				}
-
-				coord = {
-					x: event.mapCoord.x,
-					y: event.mapCoord.y,
-					groupID: event.groupID
-				};
-				console.log('navi', coord);
-
-				if (self.tools.selectUserId in self.hz_marker) {
-					var im = self.hz_marker[self.tools.selectUserId ].marker;
-					naviCoord[0] = im.getPosition();
-				} else {
-					hzInfo('请选择用户。');
-					return;
-				}
-
-				naviCoord[1] = coord;
-
-				// 判断起点和终点是否相同
-				if (naviCoord[0].x == coord.x) {
-					hzInfo('起点和终点相同，请重新选择终点。');
-					return;
-				}
-
-				self.createNavi(naviCoord);
-			}
-		});
-
-
+		this.mapClickNode();
 		
 		// 创建地图导航控制面板
 		if(options['showNavCtrlPanel']) { self.createNavigationCtrlPanel(); }
@@ -144,6 +57,119 @@
 		// --------------------------------------------------------------------
 		// 业务工具面板 功能代码 begin
 		// --------------------------------------------------------------------
+
+		// 地图加载完成回掉方法
+		loadComplete: function(){
+			var self = this;
+			this.fmap.on('loadComplete',function() {
+				self.toolControl = new fengmap.toolControl(self.fmap, {
+					// 初始化2D模式
+					init2D: false,
+					// 设置为false表示只显示2D,3D切换按钮
+					groupsButtonNeeded: false,
+					imgURL: '/static/img/',
+					// 点击按钮的回调方法,返回type表示按钮类型,value表示对应的功能值
+					clickCallBack: function(type, value) {
+						// console.log(type,value);
+					}
+				});
+
+				self.socket = io.connect(Hzlbs.HZ_CONN_STR);
+				// 增加定位代码，实时显示人物位置
+				self.socket.on('hz_position', function (msg) {
+					console.log('hz_position', msg);
+					for(var i = 0; i < msg.length; i++) {
+						var fm_coord = Hzlbs.coordMapToFMap({x: msg[i].x, y: msg[i].y});
+						self.addPeople({id: msg[i].userId, x: fm_coord.x, y: fm_coord.y});
+					}
+				});
+			});
+		},
+
+		mapClickNode: function () {
+			var self = this;
+			this.fmap.on('mapClickNode',function(event) {
+				if (self.isNavigating) { return; }
+
+				if (event.nodeType == fengmap.FMNodeType.MODEL) {
+					var modelLabel = event.label;
+					var coord;
+
+					// 如果拾取的模型没有Label对象，则使用模型中心点的坐标
+					// 有则使用与模型对应的Label对象的坐标。
+					if (!modelLabel) {
+						coord = {
+							x: event.mapCoord.x,
+							y: event.mapCoord.y,
+							groupID: event.groupID
+						};
+						console.log('no label', coord);
+					} else {
+						coord = {
+							x: modelLabel.mapCoord.x,
+							y: modelLabel.mapCoord.y,
+							groupID: event.groupID
+						};
+
+						console.log('has label', coord);
+					}
+
+					coord = {
+						x: event.mapCoord.x,
+						y: event.mapCoord.y,
+						groupID: event.groupID
+					};
+					console.log('navi', coord);
+
+					var naviCoord = [];
+					var people = self.getSelectPeople();
+					if (people) {
+						naviCoord[0] = people.getPosition();
+					} else {
+						hzInfo('请选择用户。');
+						return;
+					}
+
+					naviCoord[1] = coord;
+					self.destCoord = coord;
+
+					// 判断起点和终点是否相同
+					if (naviCoord[0].x == coord.x) {
+						hzInfo('起点和终点相同，请重新选择终点。');
+						return;
+					}
+
+					self.createNavi(naviCoord);
+				}
+			});
+		},
+
+		addPeople: function (options) {
+			options = options || {};
+			if (options && !this.userList.hasOwnProperty(options.id)) {     // 用户不存在
+				this.userList[options.id] = new HzPeople(this, options);
+				return true;
+			}
+			return false;
+		},
+
+		// 删除用户marker
+		delPeople: function (userId) {
+			var people = this.userList[userId];
+			if(people){
+				people.destroy();
+				delete this.userList[userId]
+			}
+		},
+
+		// 根据用户ID查询 HzPeople 对象
+		getPeople: function (userId) {
+			return this.userList[userId];
+		},
+
+		getSelectPeople: function () {
+			return this.getPeople(this.tools.selectUserId);
+		},
 		
 		// 创建 业务工具面板。 使用ACE框架。
 		createServicePanel: function () {
@@ -238,44 +264,48 @@
 				'<div id="' + this.navCtrlPanelId + '" style="position:absolute; top:10px; left:120px; z-index:1000; width:370px;">'+
 				'<div class="btn-group btn-group-xs col-xs-12" id="hz_panel_nav_button" style="background:#FFF; border:1px solid #CCC;">'+
 				'<button type="button" class="btn btn-pink disabled"><i class="ace-icon fa fa-road align-top bigger-125"></i>导航</button>'+
-				'<select id="hz_nav_dest" class="btn btn-pink" name="locations" style="font-family:Verdana,sans-serif;" title="目的地">'+
-				'<option value="27">Room 1 北斗羲和</option>'+
-				'<option value="29">Room 2</option>'+
-				'<option value="30">Room 3</option>'+
-				'<option value="31">Room 4 健身房</option>'+
-				'<option value="32">Room 5</option>'+
-				'<option value="33">Room 6</option>'+
-				'<option value="34">Room 7 演示厅</option>'+
-				'<option value="23">会议室</option>'+
-				'<option value="28">总裁办公室</option>'+
-				'<option value="24">副总办公室</option>'+
-				'<option value="25">仓库1</option>'+
-				'<option value="26">仓库2</option>'+
+				'<select id="hz_nav_user" class="btn btn-pink" name="locations" style="font-family:Verdana,sans-serif;" title="用户">'+
+				'<option value="0" selected >请选择用户</option>'+
+				'<option value="1918E00103AA">1918E00103AA</option>'+
+				'<option value="1918E00103A9">1918E00103A9</option>'+
 				'</select>'+
-				'<button type="button" class="btn btn-pink" id="hz_btn_begin_nav" >开始导航</button>'+
+				'<button type="button" class="btn btn-pink" id="hz_btn_begin_nav" >模拟导航</button>'+
 				'<button type="button" class="btn btn-pink" id="hz_btn_stop_nav">结束导航</button>'+
 				'</div>'+
 				'</div>'
 			);
 			var map = this;
-			
-			$("#hz_nav_dest").val(parseInt(this.tools.getDestination()));
+
+			var oUserId = $("#hz_nav_user");
+			oUserId.val(this.tools.selectUserId == '' ? '0' : this.tools.selectUserId );
+
+			oUserId.change(function () {
+				var val = $(this).val();
+
+				var curPeople = map.getSelectPeople();
+				if (curPeople) { curPeople.unselect();  }
+				var people = map.getPeople(val);
+				if (people) { people.select();  }
+
+				map.tools.setSelectUserId(val == '0' ? '' : val);   // 保存当前选择用户
+			});
 			
 			// 开始导航 button
 			$('#hz_btn_begin_nav').click(function () {
-				if(map.navi) {
-					map.navi.simulate();
+				if(!map.tools.selectUserId) {
+					hzInfo('请选择要导航的用户。');
 					return;
 				}
 
-				if(!map.tools.selectUserId) {
-					hzInfo('请在地图上选择要导航的用户。');
+				if (!map.destCoord) {
+					hzInfo('请点击地图选择<span style="color:red;font-size:16px;"><strong>目的地</strong></span>。');
 					return;
 				}
-				map.startNavigation({
-					location: $('#hz_nav_dest').val(),
-					userId: map.tools.selectUserId ||  '1918E00103AA'
-				});
+
+				if(map.navi) {
+					map.isNavigating = true;
+					map.navi.simulate();
+				}
 			});
 			
 			// 结束导航 button
@@ -304,6 +334,8 @@
 				this.fmap.tiltAngle = 30;
 				this.fmap.moveToCenter();
 			}
+			this.isNavigating = false;
+			this.destCoord = undefined;
 
 			//this.tools.setNavStatus(false);
 		},
@@ -341,7 +373,6 @@
 			});
 
 			layer.addMarker(im);
-			console.log('marker:', coord, im);
 			return im;
 		},
 
@@ -379,8 +410,9 @@
 			return fmap;
 		},
 
-		// 创建导航路线
-		createNavi: function (coord) {
+		// 创建导航路线. isShowStartPoint true，显示 起点marker； false(default)，不显示起点marker
+		createNavi: function (coord, isShowStartPoint) {
+			isShowStartPoint = (isShowStartPoint === true); // default: false
 			if (!this.navi) {
 				// 初始化导航对象
 				this.navi = new fengmap.FMNavigation({
@@ -417,7 +449,7 @@
 				x: coord[0].x,
 				y: coord[0].y,
 				groupID: coord[0].groupID,
-				url: '/static/img/start.png',
+				url: isShowStartPoint ? '/static/img/start.png': '',
 				height: 0.5,
 				size: 32
 			});
@@ -441,5 +473,106 @@
 
 	
 	};   // end of hzlbs.Hz3DMap.prototype
+
+	//-------------------------------------------------------------------------
+	// begin of HzPeople
+	//-------------------------------------------------------------------------
+	function HzPeople(map, options) {
+		options = options || {};
+		this.id = options.id;
+		this.text = options.text;
+		this.x = options.x;
+		this.y = options.y;
+		this.map = map;
+		this.img = options.img || '/static/img/blueImageMarker.png';
+		this.imageMarker = null;
+
+		this.renderer();
+
+		if(map.tools.selectUserId == this.id) { this.select();  }
+	}
+
+	HzPeople.prototype = {
+		constructor: HzPeople,  // 构造函数
+
+		getId: function () {
+			return this.id;
+		},
+
+		setText: function (text) {
+			this.text = text;
+			this.textContainer.text(text);
+		},
+
+		setPosition: function (x, y) {  // x,y 为 地图坐标
+			this.x = x;
+			this.y = y;
+
+			this.renderer();
+		},
+
+		getPosition: function () {
+			return this.imageMarker.getPosition();
+		},
+
+		renderer: function () {
+			this.imageMarker = this.addMarker();
+		},
+
+		moveTo: function (x, y) { // x,y 为 地图坐标
+
+		},
+
+		// 选中用户
+		select: function () {
+			this.imageMarker.url = '/static/img/redImageMarker.png';
+			this.map.tools.setSelectUserId(this.getId());
+		},
+
+		unselect: function () {
+			this.imageMarker.url = '/static/img/blueImageMarker.png';
+			this.map.tools.setSelectUserId('');
+		},
+
+		addMarker: function () {
+			// 获取焦点层
+			var fmap = this.map.fmap;
+			var currentGid = fmap.focusGroupID;
+			var group = fmap.getFMGroup(currentGid);
+
+			if(!group) return null;
+
+			// 返回当前层中第一个imageMarkerLayer,如果没有，则自动创建
+			var layer = group.getOrCreateLayer('imageMarker');
+
+			// 图标标注对象，默认位置为该楼层中心点
+			var im = new fengmap.FMImageMarker({
+				x: this.x,
+				y: this.y,
+
+				// 设置图片路径
+				url: this.img,
+				// 设置图片显示尺寸
+				size: 32,
+				height: 0.5,
+				callback: function() {
+					// 在图片载入完成后，设置 "一直可见"
+					im.alwaysShow();
+				}
+			});
+
+			layer.addMarker(im);
+			return im;
+		},
+
+		destroy: function () {
+			this.imgContainer.remove();
+			this.textContainer.remove();
+		}
+	};
+
+	//-------------------------------------------------------------------------
+	// end of HzPeople
+	//-------------------------------------------------------------------------
 
 })(window);
