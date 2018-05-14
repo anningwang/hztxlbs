f429281d9a770212553a6abdf90fe4d903e7d596
anningwang <wxg001@21cn.com>
2018-05-14 12:45:45 +0800

    增加 tcp server 。

--- run.py
+++ run.py
@@ -2,9 +2,14 @@
 # -*- coding:utf-8 -*-
 from app import app, socketio
 from app.utils.getip import get_ip
+from app.tcpserver import tcpserver
 
 
 if __name__ == '__main__':
     ip = get_ip()
+    print("ip=%s" % ip)
+
+    tcpserver.t.start()
+
     # app.run(host=ip, port=80, debug=True, use_reloader=False, threaded=True)
     socketio.run(app, host=ip, port=8300, debug=True, use_reloader=False)
