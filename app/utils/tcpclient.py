ac5efe9a6e4ef8ad408b2dd347e4606960b1777f
anningwang <wxg001@21cn.com>
2018-05-21 19:10:27 +0800

    修改测试客户端。

--- app/utils/tcpclient.py
+++ app/utils/tcpclient.py
@@ -1,23 +1,45 @@
 import socket
 import sys
+import time
 
 HOST, PORT = "localhost", 9999
 data = " ".join(sys.argv[1:])
 
-# Create a socket (SOCK_STREAM means a TCP socket)
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-try:
-    # Connect to server and send data
-    sock.connect((HOST, PORT))
-    if data == '':
-        data = 'hello python!'
-    sock.sendall(data + "\n")
+def client_socket(msg):
+    # Create a socket (SOCK_STREAM means a TCP socket)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # Receive data from the server and shut down
-    received = sock.recv(1024)
-finally:
-    sock.close()
+    try:
+        # Connect to server and send data
+        sock.connect((HOST, PORT))
+        if msg == '':
+            msg = 'hello python!'
+        sock.sendall(msg + "\n")
+        print "Sent:     {}".format(msg)
 
-print "Sent:     {}".format(data)
-print "Received: {}".format(received)
+        # Receive data from the server and shut down
+        received = sock.recv(1024)
+        print "Received: {}".format(received)
+    finally:
+        sock.close()
+    return
+
+
+def multi_socket(num=10000):
+    list_s = []
+    for x in xrange(0, num):
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((HOST, PORT))
+            list_s.append(s)
+            time.sleep(0.01)
+            print x
+        except BaseException, e:
+            print "exception: ", type(e), e
+        time.sleep(0.5)
+    return
+
+
+client_socket('send from client socket.')
+multi_socket(200)
